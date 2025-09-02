1fe70c6a562a6628604d88c1a252daf2babafd08
argonism <44169535+argonism@users.noreply.github.com>
2025-09-02 21:14:37 +0900

    Add indent and warnings for gfm

--- tests/elements/horizontal_test.py
+++ tests/elements/horizontal_test.py
@@ -16,9 +16,26 @@ def test_md_horizontal_custom_content() -> None:
     ), f"Content should be '{custom_content}'"
 
 
-def test_md_horizontal_non_str_content() -> None:
-    with pytest.warns(UserWarning, match="Horizontal content is not a string"):
-        md_horizontal = MdHorizontal(content=123)  # type: ignore
+
+def test_md_horizontal_invalid_character() -> None:
+    with pytest.warns(UserWarning, match="Horizontal content is not a valid character"):
+        md_horizontal = MdHorizontal(content="+++")
         assert (
-            str(md_horizontal) == "\n123\n"
-        ), "non str content should be casted to str"
+            str(md_horizontal) == "\+++\n"
+        ), "invalid character content should still be accepted"
+
+
+def test_md_horizontal_short_content() -> None:
+    with pytest.warns(UserWarning, match="Horizontal content not long enough"):
+        md_horizontal = MdHorizontal(content="**")
+        assert (
+            str(md_horizontal) == "\n**\n"
+        ), "short content should still be accepted"
+
+
+def test_md_horizontal_long_indent() -> None:
+    with pytest.warns(UserWarning, match="Horizontal indent too long"):
+        md_horizontal = MdHorizontal(indent="    ")
+        assert (
+            str(md_horizontal) == "\n   ***\n"
+        ), "indent should be truncated to 3 spaces"
